3703c9cbd2d7ec2d747b32665cdfb321f4f94c7e
Michael <mammoliti.michael@gmail.com>
2015-11-17 10:25:58 +0100

    A lot of changes
    - More robust and maintainable
    - Better OOP structure (better prototype approach)
    - Better properties and methods names
    - jQuery plugin
    - Lot of bug fixes

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,266 +1,306 @@
-(function( undefined )
+var pluginName  = "calculator",
+    defaults    = {};
+
+function Plugin(context, options)
 {
-  function Calculator(context)
+  this.settings     = $.extend({}, options, defaults);
+
+  this.$context         = $(context);
+  this.$display         = this.$context.find("#display");
+  this.$keys            = this.$context.find("#keys");
+  this.$memoryKey       = this.$keys.find("#memorykey");
+  this.$resetKey        = this.$keys.find("#resetkey");
+  this.$memoryKey       = this.$keys.find("#memorykey");
+  this.firstNum         = undefined;
+  this.secondNum        = undefined;
+  this.floatNum         = false;
+  this.currentOperator  = undefined;
+  this.writingState     = undefined;
+  this.prevKey          = undefined;
+  this.keyType          = undefined;
+  this.keyValue         = undefined;
+  this.memory           = undefined;
+
+  this.init();
+}
+
+Plugin.prototype = {
+
+
+  init: function()
   {
     var self = this;
 
-    self.$context   = $(context),
-    self.$keys      = self.$context.find("button"),
-    self.$display   = self.$context.find("#display"),
+    this.events();
+    this.utilities.debug.call(self);
+  },
 
-    self.$ACKey     = self.$context.find("#resetkey"),
-    self.$memoryKey = self.$context.find("#memorykey"),
-
-    self.currentOperator,
-    self.firstNum,
-    self.secondNum,
-    self.writingState,
-    self.prevKey,
-    self.memory,
-    self.percentage,
-
-    self.keyType,
-    self.keyValue,
-    self.displayText,
-
-    self.debug      = true;
-  }
-
-  Calculator.prototype.reset = function()
+  events: function()
   {
+
     var self = this;
 
-    self.firstNum        = undefined;
-    self.secondNum       = undefined;
-    self.memory          = undefined;
-    self.percentage      = undefined;
-
-    self.currentOperator = undefined;
-    self.prevKey         = undefined;
-
-    self.keyType         = undefined;
-    self.writingState    = undefined;
-    self.displayText     = undefined;
-
-    // reset keys
-    self.$display.text("0");
-    self.$memoryKey.text("M+");
-    self.$ACKey.text("AC");
-  }
-
-  Calculator.prototype.clear = function()
-  {
-    var self = this;
-
-    self.displayText = "0";
-    self.$display.text("0");
-    self.$ACKey.text("AC");
-  }
-
-
-  Calculator.prototype.calculate = function(n1, n2)
-  {
-    var self  = this,
-        n1    = parseFloat(n1),
-        n2    = parseFloat(n2),
-        tot;
-
-    switch(self.currentOperator)
-    {
-      case "+":
-        tot = n1 + n2;
-        break;
-
-      case "-":
-        tot = n1 - n2;
-        break;
-
-      case "/":
-        tot = n1 / n2;
-        break;
-
-      case "*":
-        tot = n1 * n2;
-        break;
-    }
-
-    return tot;
-  }
-
-  Calculator.prototype.handleKey = function($btn)
-  {
-    var self = this;
-
-    function handleNumber($btn)
-    {
-      if(self.prevKey === "=")
-      {
-        self.firstNum      = undefined;
-        self.secondNum     = undefined;
-        self.displayText   = "0";
-        self.$display.text("0");
-      }
-
-      if(self.displayText === "0" || self.writingState === "operator") { self.displayText = ""; }
-      if(self.keyValue === ".") { self.displayText = "0"; }
-
-      self.$display.html(self.displayText + self.keyValue);
-    }
-
-    function handleOperator($btn)
-    {
-      if(!self.currentOperator){ self.currentOperator = self.keyValue; }
-
-      if(self.writingState === "operator")
-      {
-        console.log("here");
-        self.currentOperator = self.keyValue;
-        return;
-      }
-
-      if(self.prevKey === "=")
-      {
-        self.secondNum = undefined;
-        self.currentOperator = self.keyValue;
-        return;
-      }
-
-      if(self.firstNum === undefined)
-      {
-        self.firstNum = self.displayText;
-      }
-        else if(self.secondNum === undefined)
-        {
-          self.secondNum = self.displayText;
-
-          var tot = self.calculate(self.firstNum, self.secondNum);
-
-          self.firstNum  = tot;
-          self.secondNum = undefined;
-          self.$display.text(tot);
-        }
-
-      self.currentOperator = self.keyValue;
-    }
-
-    function handleFunction($btn)
-    {
-
-      switch(self.keyValue)
-      {
-        // - Reset Calc
-        case "AC":
-          self.reset();
-          break;
-
-        // Clear text only
-        case "C":
-          self.clear();
-          break;
-
-        // Convert to negative
-        case "+/-":
-          var num = self.displayText * -1;
-          self.displayText = num;
-          self.$display.text(num);
-          break;
-
-        // Do Math
-        case "=":
-          if(self.secondNum === undefined) self.secondNum = self.displayText;
-          var tot       = self.calculate(self.firstNum, self.secondNum);
-          self.firstNum = tot;
-          self.$display.text(tot);
-          break;
-
-        // Memory
-        case "M+":
-          $btn.text("M-");
-          self.$display.text("0");
-          self.memory = self.displayText;
-          break;
-
-        // Unmemorize
-        case "M-":
-          $btn.text("M+");
-          self.$display.text(self.memory);
-          self.memory = undefined;
-          break;
-
-        // - Percentage
-        case "%":
-          self.percentage  = (self.firstNum / 100) * self.displayText;
-          self.displayText = self.percentage;
-          self.$display.text(self.percentage);
-          break;
-      }
-    }
-
-    switch(self.keyType)
-    {
-      case "number":
-        handleNumber($btn);
-        break;
-
-      case "operator":
-        handleOperator($btn);
-        break;
-
-      case "function":
-        handleFunction($btn);
-        break;
-    }
-
-  }
-
-  Calculator.prototype.log = function()
-  {
-    var self  = this,
-        d     = {
-          "firstNum"        : self.firstNum,
-          "secondNum"       : self.secondNum,
-          "memory"          : self.memory,
-          "percentage"      : self.percentage,
-          "currentOperator" : self.currentOperator,
-          "prevKey"         : self.prevKey,
-          "keyType"         : self.keyType,
-          "writingState"    : self.writingState,
-          "displayText"     : self.displayText
-        };
-
-    if(self.debug) console.log(d);
-  }
-
-  Calculator.prototype.initialize = function()
-  {
-    var self = this;
-
-    self.$context.on("click", "button", function()
+    self.$keys.on("click", "button", function()
     {
       var $btn = $(this);
 
-      // - Set Variables
-      self.keyType     = $btn.data("type");
-      self.keyValue    = $btn.text();
-      self.displayText = self.$display.text();
+      self.keyType      = $btn.data("type");
+      self.keyValue     = $btn.text();
 
-      // - Change keys
-      self.$ACKey.text("C");
+      self.$resetKey.text("C");
 
-      // - Handle key
-      self.handleKey($btn);
+      switch(self.keyType)
+      {
+        case "number":    self.handle.num.call(self);       break;
+        case "operator":  self.handle.operator.call(self);  break;
+        case "function":  self.handle.func.call(self);      break;
+      }
 
-      // - Set Variables
-      self.writingState  = self.keyType;
-      self.prevKey       = self.keyValue;
+      if(self.keyValue === "AC") return;
 
-      self.log();
+      self.writingState = self.keyType;
+      self.prevKey      = self.keyValue;
+
+      self.utilities.debug.call(self);
 
     });
+
+  },
+
+  calculate: function(num1, num2)
+  {
+    var self = this,
+        tot;
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch(self.currentOperator)
+    {
+      case "+": tot = self.operations.addition(num1, num2);       break;
+      case "-": tot = self.operations.subtraction(num1, num2);    break;
+      case "*": tot = self.operations.multiplication(num1, num2); break;
+      case "/": tot = self.operations.division(num1, num2);       break;
+    }
+
+    return tot;
+  },
+
+  render: function(text)
+  {
+    var self  = this;
+
+    self.$display.text(text);
+
+    return text;
+  },
+
+  handle: {
+    num: function()
+    {
+      var self  = this,
+          num   =  self.$display.text();
+
+      // if clicked operator
+      if(self.writingState === "operator") num = "0";
+      if(self.prevKey === "=")
+      {
+        num             = "0";
+        self.firstNum   = undefined;
+        self.secondNum  = undefined;
+      }
+
+      // make a clean number
+      if(num === "0") num = "";
+
+      // make float number
+      if(self.keyValue === ".")
+      {
+        if(num == "") num = "0";
+        if(self.floatNum) return;
+        self.floatNum = true;
+      }
+
+      self.render(num + self.keyValue);
+    },
+
+    operator: function()
+    {
+      var self        = this,
+          displayText = parseFloat( self.$display.text() ),
+          tot;
+
+      self.floatNum = false;
+
+      if(!self.currentOperator) self.currentOperator = self.keyValue;
+
+      // change operator only
+      if(self.writingState === "operator")
+      {
+        self.writingState = self.keyValue;
+        return;
+      }
+
+      if(self.prevKey === "=")
+      {
+        self.currentOperator  = self.keyValue;
+        self.secondNum        = undefined;
+        self.firstNum         = displayText;
+        return;
+      }
+
+      // setting numbers
+      if(self.firstNum === undefined)
+      {
+        self.firstNum = displayText;
+      }
+        else if(self.secondNum === undefined)
+        {
+          self.secondNum  = displayText;
+          tot             = self.calculate(self.firstNum, self.secondNum);
+
+          self.render(tot);
+
+          self.firstNum   = tot;
+          self.secondNum  = undefined;
+        }
+
+      self.currentOperator = self.keyValue;
+    },
+
+    func: function()
+    {
+      var self = this;
+
+      switch(self.keyValue)
+      {
+        case "=": self.functions.getResult.call(self); break;
+        case "M+": self.functions.memorize.call(self); break;
+        case "M-": self.functions.unmemorize.call(self); break;
+        case "%": self.functions.percentage.call(self); break;
+        case "+/-": self.functions.convertPolarity.call(self); break;
+        case "AC": self.functions.resetCalc.call(self); break;
+        case "C": self.functions.clearDisplay.call(self); break;
+      }
+    }
+  },
+
+  operations: {
+    addition:       function(num1, num2){return num1 + num2;},
+    subtraction:    function(num1, num2){return num1 - num2;},
+    multiplication: function(num1, num2){return num1 * num2;},
+    division:       function(num1, num2){return num1 / num2;}
+  },
+
+  functions: {
+    resetCalc: function()
+    {
+      var self = this;
+
+      self.$resetKey.text("AC");
+      self.$display.text("0");
+
+      self.firstNum         = undefined;
+      self.secondNum        = undefined;
+      self.keyValue         = undefined;
+      self.keyType          = undefined;
+      self.currentOperator  = undefined;
+      self.floatNum         = false;
+      self.memory           = undefined;
+    },
+
+    clearDisplay: function()
+    {
+      var self = this;
+
+      self.$resetKey.text("AC");
+      self.$display.text("0");
+    },
+
+    memorize: function()
+    {
+      var self  = this,
+          m     = self.$display.text();
+
+      self.memory = parseFloat( m );
+      self.$display.text("0");
+      self.$memoryKey.text("M-");
+
+      return m;
+    },
+
+    unmemorize: function()
+    {
+      var self  = this,
+          m     = self.memory;
+
+      self.$display.text(m);
+      self.memory = undefined;
+      self.$memoryKey.text("M+");
+
+      return m;
+    },
+
+    convertPolarity: function()
+    {
+      var self  = this,
+          num   = self.$display.text();
+
+      self.$display.text(num*-1);
+
+      return num*-1;
+    },
+
+    percentage: function()
+    {
+      var self        = this,
+          percentage  = (self.firstNum / 100) * self.$display.text();
+
+      self.$display.text( percentage );
+
+      return percentage;
+    },
+
+    getResult: function()
+    {
+      var self        = this,
+          displayText = parseFloat( self.$display.text() ),
+          tot;
+
+      if(self.secondNum === undefined) self.secondNum = displayText;
+
+      tot = self.calculate(self.firstNum, self.secondNum);
+      self.render(tot);
+
+      self.firstNum = tot;
+
+      return tot;
+    }
+  },
+
+  utilities: {
+    debug: function()
+    {
+      var self = this,
+          o;
+
+      if(!self.settings.debug) return;
+
+      o = self;
+
+      console.log( o );
+      return o;
+    }
   }
 
-  // - Initialize Script
-  var c = new Calculator("#calculator");
-  c.initialize();
+}
 
-}(jQuery));
+$.fn[pluginName] = function(options)
+{
+  $(this).each(function()
+  {
+    return new Plugin(this, options)
+  })
+}
+
+$("#calculator").calculator();
